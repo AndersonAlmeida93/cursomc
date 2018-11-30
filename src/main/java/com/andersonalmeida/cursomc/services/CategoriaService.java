bc4e16b01d3535bc5dc9a9d3ccbf03b62c234766
Anderson Almeida <andersonolld@gmail.com>
2018-11-29 23:32:59 -0300

    Criando repository e service para Categoria.

---
+++ src/main/java/com/andersonalmeida/cursomc/services/CategoriaService.java
@@ -0,0 +1,19 @@
+package com.andersonalmeida.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andersonalmeida.cursomc.domain.Categoria;
+import com.andersonalmeida.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria buscar(Integer id) {
+		Categoria obj = repo.findOne(id);
+		return obj;
+	}
+}
