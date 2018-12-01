0c5c755ef075971186f68049a9be00e57710b3a5
Anderson Almeida <andersonolld@gmail.com>
2018-12-01 18:47:14 -0300

    Endpoin /clientes/{id} disponivel.

---
+++ src/main/java/com/andersonalmeida/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.andersonalmeida.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andersonalmeida.cursomc.domain.Cliente;
+import com.andersonalmeida.cursomc.repositories.ClienteRepository;
+import com.andersonalmeida.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findOne(id);
+		
+		if(obj == null){
+			throw new ObjectNotFoundException("Ojeto não encontrado! Id:" + id +  " , Tipo: " + Cliente.class.getName());
+		}
+		return obj;
+	}
+}
