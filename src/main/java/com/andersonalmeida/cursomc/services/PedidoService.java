4a5d9a392315c2981c28efecc64a213b66d0cda5
Anderson Almeida <andersonolld@gmail.com>
2018-12-01 22:45:02 -0300

    Endpoint /pedidos/{id} disponibilizados.

---
+++ src/main/java/com/andersonalmeida/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.andersonalmeida.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andersonalmeida.cursomc.domain.Pedido;
+import com.andersonalmeida.cursomc.repositories.PedidoRepository;
+import com.andersonalmeida.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Pedido obj = repo.findOne(id);
+		
+		if(obj == null){
+			throw new ObjectNotFoundException("Ojeto não encontrado! Id:" + id +  " , Tipo: " + Pedido.class.getName());
+		}
+		return obj;
+	}
+}
