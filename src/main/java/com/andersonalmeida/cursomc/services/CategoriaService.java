25d22a76bce8f9808359d4c46aaa77b52e71a419
Anderson Guilherme da Silva Almeida <anderson.almeida@pampa.compasso>
2020-03-13 13:56:16 -0300

    Paginação com parametros opcionais na requisição.

--- src/main/java/com/andersonalmeida/cursomc/services/CategoriaService.java
+++ src/main/java/com/andersonalmeida/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.andersonalmeida.cursomc.domain.Categoria;
@@ -53,6 +56,13 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível exlcuir uma categoria que possui produtos.");
 		}
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+		
 	}
 	
 	
