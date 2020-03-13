8fc4b8063685c966a013967611317da30d113914
Anderson Guilherme da Silva Almeida <anderson.almeida@pampa.compasso>
2020-03-13 10:27:52 -0300

    Atualizado Categoria com PUT e outros pequenos ajustes.

--- src/main/java/com/andersonalmeida/cursomc/services/CategoriaService.java
+++ src/main/java/com/andersonalmeida/cursomc/services/CategoriaService.java
@@ -13,7 +13,8 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
+		
 		Categoria obj = repo.findOne(id);
 		
 		if(obj == null){
@@ -24,7 +25,14 @@ public class CategoriaService {
 	}
 	
 	public Categoria insert(Categoria obj) {
+		
 		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		
+		find(obj.getId());
 		return repo.save(obj);
 	}
 }
